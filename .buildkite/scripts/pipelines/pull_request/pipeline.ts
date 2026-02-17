99a287baa9724320f268b5a99f5fbcc7878112ac
Brad White <brad.white@elastic.co>
2026-02-17 14:51:00 +1000

    Revert "fake commit"
    This reverts commit e984fea6942a5569d60c09481999a21712705b95.

--- .buildkite/scripts/pipelines/pull_request/pipeline.ts
+++ .buildkite/scripts/pipelines/pull_request/pipeline.ts
@@ -26,7 +26,6 @@ import {
   prHasFIPSLabel,
 } from '#pipeline-utils';
 
-// TODO: fake commit, remove after
 const prConfig = prConfigs.jobs.find((job) => job.pipelineSlug === 'kibana-pull-request');
 const emptyStep = `steps: []`;
 
